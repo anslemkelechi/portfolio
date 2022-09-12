7951e9ff60f6cccd5b58aa4513643a6fd5ade9a2
Blakcoder <anslemkay180@gmail.com>
2022-09-12 04:27:09 +0100

    v!

---
+++ app.js
@@ -0,0 +1,13 @@
+// Settings for mouse js
+options = {
+  cursorOuter: 'circle-basic',
+  hoverEffect: 'pointer-blur',
+  hoverItemMove: false,
+  defaultCursor: false,
+  outerWidth: 60,
+  outerHeight: 60,
+}
+magicMouse(options)
+
+// Initialize AOS
+AOS.init()
